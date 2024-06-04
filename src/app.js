9babddb0c705f148ff798b9a6886da21ff312707
EberG25 <168913317+EberG25@users.noreply.github.com>
2024-06-04 00:32:50 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -9,3 +9,18 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronouns = ["the", "our"];
+let adjs = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+
+const domain = [];
+for (const i in pronouns) {
+  for (const j in adjs) {
+    for (const k in nouns) {
+      const d = pronouns[i] + adjs[j] + nouns[k] + ".com";
+      domain.push(d);
+    }
+  }
+}
+console.log(domain);
